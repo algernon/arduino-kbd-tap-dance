cedebf34f166112a857bdbe373bd21759fc17cd6
Gergely Nagy <algernon@madhouse-project.org>
2016-09-17 10:26:05 +0200

    New test
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/test.cpp
+++ src/test.cpp
@@ -122,11 +122,42 @@ test_one_tap_with_interrupt_after () {
   assert (t.is_interrupted () == false);
 }
 
+void
+test_one_tap_with_interrupt_during_release () {
+  TapDanceTestKey t = TapDanceTestKey(42);
+
+  // Tapping the key once should increment the counter by one
+  t.press (42);
+  assert (t.get_count () == 1);
+  // The key is also pressed now
+  assert (t.is_pressed ());
+  // The onEach callback gets called, once
+  assert (t.cnt_onEach == 1);
+
+  // Tapping another key shall interrupt the dance
+  t.press (13);
+
+  // onEach should still be one, because we did not tap the dance-key
+  assert (t.cnt_onEach == 1);
+  // finish has to be called once, because we finished with the key; interrupt also set
+  assert (t.cnt_onFinish == 1);
+  assert (t.cnt_Interrupted == 1);
+  // reset should also be called, because of the interrupt
+  assert (t.cnt_onReset == 1);
+
+  // at this point, everything should be reset to default, save our test counters
+  assert (t.get_count () == 0);
+  assert (t.is_pressed () == false);
+  assert (t.is_finished () == false);
+  assert (t.is_interrupted () == false);
+}
+
 int
 main (void) {
 
   test_tapping_on_a_single_key ();
   test_one_tap_with_interrupt_after ();
+  test_one_tap_with_interrupt_during_release ();
 
   return 0;
 }
